fdea4454bfc5aba9a59056b947659271b8804176
Viacheslav Knyazev <viacheslav.knyazev@canada.ca>
2018-08-07 17:37:31 +0000

    Added MiniTex and Row Spanning

--- TagHelpers/MiniTex.cs
+++ TagHelpers/MiniTex.cs
@@ -6,17 +6,28 @@ using System.Text.RegularExpressions;
 
 namespace Infobase.TagHelpers
 {
-    public class MiniTex : TagHelper
+    public class EmailTagHelper : TagHelper
     {
-        public override async void Process(TagHelperContext context, TagHelperOutput output)
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
+            output.TagName = "span";    // Replaces <email> with <a> tag
             var content = await output.GetChildContentAsync();
-            output.TagName = "span";
-            output.Content.SetContent(MiniTex.Parse(content.GetContent()));
+            output.Content.SetHtmlContent(MiniTexTagHelper.Parse(content.GetContent(NullHtmlEncoder.Default)));
+        }
+    }
+
+    [HtmlTargetElement("MiniTex")]
+    public class MiniTexTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";    // Replaces <email> with <a> tag
+            var content = await output.GetChildContentAsync();
+            output.Content.SetHtmlContent(MiniTexTagHelper.Parse(content.GetContent(NullHtmlEncoder.Default)));
         }
         public static string Parse(string data) {
             // apply superscript
-            data = Regex.Replace(WebUtility.HtmlEncode("hi <script></script>superscript{at}e"), "superscript{([a-zA-Z0-9]+)}", "<sup>$1</sup>");
+            data = Regex.Replace(data, "\\\\superscript{([a-zA-Z0-9]+)}", "<sup>$1</sup>");
             return data;
         }
     }
