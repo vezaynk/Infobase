68cdf15beeb284048cc4c405a2e8abc6ec35b175
Viacheslav Knyazev <viacheslav.knyazev@canada.ca>
2018-08-07 20:08:08 +0000

    back-end formatting

---
+++ Common/Formatter.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Globalization;
+
+namespace Infobase
+{
+    public static class Formatter {
+        public static string Standard(double format, string local) {
+            CultureInfo myCI = new CultureInfo(local, false);
+            return String.Format(myCI, "{0:0.00}", format);
+        }
+    }
+}
