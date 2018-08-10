5a3103b42f5e9516d29e00864a00aefbe66de75b
Viacheslav Knyazev <viacheslav.knyazev@canada.ca>
2018-08-10 14:55:11 +0000

    Env-based ports

--- Program.cs
+++ Program.cs
@@ -12,14 +12,20 @@ namespace Infobase
 {
     public class Program
     {
+        private static int Port {get;set;} = 8080;
         public static void Main(string[] args)
         {
+            string portEnv = System.Environment.GetEnvironmentVariable("PORT");
+            int n = 8080;
+            bool isNumeric = int.TryParse(portEnv, out n);
+            if (isNumeric)
+                Program.Port = n;
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://0.0.0.0:8080/")
+                .UseUrls("http://0.0.0.0:" +  Program.Port.ToString())
                 .UseStartup<Startup>();
     }
 }
